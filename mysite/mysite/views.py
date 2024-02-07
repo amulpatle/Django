7275c7bb0f99c9ef96359aa8c684271a330d8588
amul patle <amulpatle3@gmail.com>
2024-02-07 11:25:50 +0530

    run if else code

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -10,6 +10,7 @@ def homePage(request):
         'title':'hey Page',
         'para':'this is new (^ ^)',
         'clist':['PHP','JAVA','Django'],
+        'numbers' : [10,20,30,40,50],
         'student_details':[
             {'name':'Amul','Phone':7222999446},
             {'name':"Atul",'Phone':8839104827}
