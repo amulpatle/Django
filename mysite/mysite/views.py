ef25f008b4c1ae4873f513e65fcb210e20659b86
amul patle <amulpatle3@gmail.com>
2024-02-07 10:29:15 +0530

    run for loop

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -8,6 +8,12 @@ def aboutUs(request):
 def homePage(request):
     data = {
         'title':'hey Page',
+        'para':'this is new (^ ^)',
+        'clist':['PHP','JAVA','Django'],
+        'student_details':[
+            {'name':'Amul','Phone':7222999446},
+            {'name':"Atul",'Phone':8839104827}
+        ]
     }
     return render(request,"index.html",data)
 
